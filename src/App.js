0b48cee563830b3b14181b902117a455d7859891
Collins <emiomacollins96@gmail.com>
2021-07-08 18:04:03 +0100

    created color pallet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './css/App.scss';
 import 'normalize-css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<Switch>
+			<Route exact path="/" component={Home} />
+		</Switch>
+	);
 }
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { HomeContainer } from './HomeStyles';
+
+function Home() {
+	return <HomeContainer></HomeContainer>;
+}
+
+export default Home;
