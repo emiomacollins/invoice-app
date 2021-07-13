94354db48529059f61342f1e338ddaca82f9f564
Collins <emiomacollins96@gmail.com>
2021-07-13 12:29:51 +0100

    empty invoiceList message component

--- src/components/Home/NewUserMessage.jsx
+++ src/components/Home/NewUserMessage.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import illustrationPath from '../../assets/images/illustration-empty.svg';
+
+const Container = styled.div`
+	display: grid;
+	place-content: center;
+	min-height: calc(90vh - var(--nav-height) * 3);
+	justify-items: center;
+	gap: 2rem;
+	max-width: 300px;
+	margin: auto;
+	text-align: center;
+`;
+
+const Illustration = styled.img`
+	width: 250px;
+`;
 
 function NewUserMessage() {
-	return <div>No invoices, start by adding an invoice</div>;
+	return (
+		<Container>
+			<Illustration src={illustrationPath} alt="" />
+			<h2>There is nothing here</h2>
+			<p>
+				Create an invoice by clicking the <span className="bold">New</span> button
+				and get started.
+			</p>
+		</Container>
+	);
 }
 
 export default NewUserMessage;
